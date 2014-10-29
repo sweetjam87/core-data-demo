55a9a6c2da4b2f775376dbcfc15d167d65d95060
James McPherson <mcphej2@Michaels-MacBook-Pro.local>
2014-10-29 15:12:51 -0500

    COREDATA-001 #comment (1) Created XCODE Project for learning and practicing CoreData.

---
+++ core-data-demo/core-data-demo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  core-data-demo
+//
+//  Created by James McPherson on 10/29/14.
+//  Copyright (c) 2014 James McPherson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ core-data-demo/core-data-demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  core-data-demo
+//
+//  Created by James McPherson on 10/29/14.
+//  Copyright (c) 2014 James McPherson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
